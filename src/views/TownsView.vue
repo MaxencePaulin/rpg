0b3b4c39d4638f9ebb7232fce3a38bf70f02ec06
MaxencePaulin <maxence.paulin@edu.univ-fcomte.fr>
2022-11-14 15:07:14 +0100

    3 du tp2

--- src/views/TownsView.vue
+++ src/views/TownsView.vue
@@ -1,10 +1,18 @@
 <template>
   <v-container>
     <h1>Les villes</h1>
-    <label for="filtertown">filtre : </label><input v-model="filter" id="filtertown">
-    <ul>
-      <li v-for="(ville, index) in villesFiltre" :key="index">{{ ville.nom }}</li>
-    </ul>
+    <label for="filteractive">filtrage possible : </label><input type="checkbox" v-model="filterActive" id="filteractive">
+    <div v-if="filterActive">
+      <label for="filtertown">filtre : </label><input v-model="filter" id="filtertown">
+      <ul>
+        <li v-for="(ville, index) in villesFiltre" :key="index">{{ ville.nom }}</li>
+      </ul>
+    </div>
+    <div v-else>
+      <ul>
+        <li v-for="(ville, index) in villes" :key="index">{{ ville.nom }}</li>
+      </ul>
+    </div>
   </v-container>
 </template>
 
