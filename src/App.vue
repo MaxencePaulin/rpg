816d4bfcce86e2f42dbe07b5c3e86538793c81c0
MaxencePaulin <maxence.paulin@edu.univ-fcomte.fr>
2022-12-05 15:27:10 +0100

    tp3 2./ fini

--- src/App.vue
+++ src/App.vue
@@ -5,36 +5,11 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <NavBar :titles="[
+          {text: 'Personnages', color: 'red'},
+          {text: 'Ville', color: 'yellow'}
+        ]" @menu-clicked='menuClicked'
+      />
     </v-app-bar>
 
     <v-main>
@@ -46,6 +21,7 @@
 <script>
 
 import {mapActions} from 'vuex'
+import NavBar from '@/components/NavBar.vue'
 
 export default {
   name: 'App',
@@ -53,9 +29,20 @@ export default {
   data: () => ({
     //
   }),
+  components: {
+    NavBar
+  },
   methods: {
     ...mapActions(['getAllTowns']),
-    ...mapActions(['getAllCharacs'])
+    ...mapActions(['getAllCharacs']),
+    menuClicked(idx) {
+      console.log('menuClicked', idx)
+      if (idx === 0) {
+        this.$router.push('/persos')
+      }else if (idx === 1) {
+        this.$router.push('/towns')
+      }
+    }
   },
   mounted() {
     this.getAllTowns();
---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <span v-for="(title, index) in titles" :key="index">
+      <v-btn :color="title.color" @click="$emit('menu-clicked', index)">
+        {{title.text}}
+      </v-btn>
+    </span>
+   <!-- button list, values/colors from props titles. -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  props: { titles: Array }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.v-btn {
+  margin: 5px;
+}
+</style>
